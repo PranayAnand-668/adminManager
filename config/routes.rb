34d870cc9af3b00f276b211428509e09ef6383d1
EthanHunt-668 <pranayanand668@gmail.com>
2025-01-04 00:32:58 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "home/about"
+   root "home#index"
+  
+end
